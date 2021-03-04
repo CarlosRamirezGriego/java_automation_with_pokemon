32c4396a459b97653b23aa7a0e293191145daf77
CarlosRG <cramirez.griego@gmail.com>
2021-03-03 19:38:43 -0700

    Created WebPageWrapper

---
+++ src/AutomationClasses/WebPageWrapper.java
@@ -0,0 +1,50 @@
+package AutomationClasses;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebPageWrapper {
+    WebDriver driver;
+    public int ImplicitWaitSeconds = 0;
+    public int TimeOutSeconds = 0;
+    public int explicitWait = 10;
+    public int milisecondsInterval = 100;
+
+    public WebPageWrapper()
+    {
+
+    }
+
+    public WebPageWrapper(WebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    public WebPageWrapper(String browser)
+    {
+        OpenBrowser(browser);
+    }
+
+    public void OpenBrowser(String browser)
+    {
+        switch (browser)
+        {
+            case "gc":
+                driver = new ChromeDriver();
+                break;
+            case "ff":
+                driver = new FirefoxDriver();
+                break;
+            default:
+                driver = new ChromeDriver();
+                break;
+
+        }
+    }
+
+    public void LoadWebPage(String url)
+    {
+        driver.get(url);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import AutomationClasses.WebPageWrapper;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello World !");
+        System.out.println("Hello World");
+        WebPageWrapper wp = new WebPageWrapper("gc");
+        wp.LoadWebPage("http://www.google.com");
     }
 }
